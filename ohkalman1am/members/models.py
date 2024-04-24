028f67a29a2cb9cc595f9796a9e8a1e48f8ca9cf
Jasper <gomezjasper123@gmail.com>
2024-04-24 11:12:31 +0800

    pushed two folders

---
+++ ohkalman1am/members/models.py
@@ -0,0 +1,7 @@
+# This imports models module form the Django package
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+   firstname = models.CharField(max_length=255)
+   lastname = models.CharField(max_length=255)
