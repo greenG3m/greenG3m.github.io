f8a396e474b656b1697b09c9b3e4acf631bb097b
Jasper <gomezjasper123@gmail.com>
2024-05-08 13:08:16 +0800

    Update: models.py

--- my_app_n1am/app/models.py
+++ my_app_n1am/app/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Member(models.Model):
    name=models.CharField(max_length=255)
    description=models.TextField()
-   def _str_(self):
+   def __str__(self):
       return self.name
